06036159d1178a10516c9fd83b60b875ce30f5de
SAMAKE MOHAMED <39623776+youzerssif@users.noreply.github.com>
2023-07-21 03:59:57 +0000

    Add button, add style to app and add like functionality

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,13 @@ import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+// Point d'entree du programme
 void main() {
   runApp(MyApp());
 }
+
+// Tout est widget et MyApp est le widget qui appel et configure tous les autres widget, le nom de l'app un peu comme le setting
+// Ici il dit que le widget principal, celui qui s'affiche quand on lance l'app c'est MyHomePage (voir ligne 26)
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -25,21 +29,98 @@ class MyApp extends StatelessWidget {
   }
 }
 
+// MyAppState détermine les données dont l'application a besoin pour fonctionner.
+
 class MyAppState extends ChangeNotifier {
+  
   var current = WordPair.random();
+  var favorites = <WordPair>[];
+
+  void getNext() {
+    current = WordPair.random();
+    notifyListeners();
+  }
+
+  void toggleFavorite() {
+    if (favorites.contains(current)) {
+      favorites.remove(current);
+    } else {
+      favorites.add(current);
+    }
+    notifyListeners();
+  }
+
 }
+
 
 class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+
     var appState = context.watch<MyAppState>();
+    var pair = appState.current;
+
+    IconData icon;
+    if (appState.favorites.contains(pair)) {
+      icon = Icons.favorite;
+    } else {
+      icon = Icons.favorite_border;
+    }
 
     return Scaffold(
-      body: Column(
-        children: [
-          Text('A random idea:'),
-          Text(appState.current.asLowerCase),
-        ],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            BigCard(pair: pair),
+            SizedBox(height: 10),
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                ElevatedButton.icon(
+                  onPressed: () {
+                    appState.toggleFavorite();
+                  },
+                  icon: Icon(icon),
+                  label: Text('Like'),
+                ),
+                SizedBox(width: 10),
+                ElevatedButton(onPressed: (){
+                  appState.getNext();
+                }, child: Text('next')),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class BigCard extends StatelessWidget {
+  const BigCard({
+    super.key,
+    required this.pair,
+  });
+
+  final WordPair pair;
+  
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context); 
+    final style = theme.textTheme.displayMedium!.copyWith(
+      color: theme.colorScheme.onPrimary,
+    );
+    return Card(
+      color: theme.colorScheme.primary, 
+      child: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Text(
+          pair.asLowerCase,
+          style: style,
+          semanticsLabel: "${pair.first} ${pair.second}",
+        ),
       ),
     );
   }
